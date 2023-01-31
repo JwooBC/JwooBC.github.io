2379c69cdd7d15905b9d85a9fe78b905fbc72b92
JwooBC <121982757+JwooBC@users.noreply.github.com>
2023-01-30 21:53:13 -0800

    Update javaScript.js

--- Modules/m05/javaScript.js
+++ Modules/m05/javaScript.js
@@ -1,5 +1,5 @@
 var table = prompt("Pick a number from 0 to 10"); // Unit of table
-var i = 0; // Set counter to 1
+var i = 1; // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>'; // Message
 
 // Do multiplication
