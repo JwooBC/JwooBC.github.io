e85bed1c0a93ef1ddba619232293327de2cb6d86
JwooBC <121982757+JwooBC@users.noreply.github.com>
2023-02-17 23:13:39 -0800

    Create javaScript.js

---
+++ Artifacts/Artifact4/javaScript.js
@@ -0,0 +1,13 @@
+function validateForm(){
+  Console.Log("Hello");
+//1) create a variable to control status of each field. Assume that they are not valid
+
+//2) create variables to read the values from html text inputs
+
+//3) do the validation
+
+//4) send error messages 
+
+//5) return the status of each field
+  return "jello";
+}
